9cfbc9aeddfffe4e8928edb1141dbf97a9ea050f
Shahrukh Yousafzai <shahrukhhkhan2003@gmail.com>
2025-07-12 03:15:05 +0000

    I think on portfolio items hover there should be dropshadow of theme ora

--- src/components/sections/portfolio.tsx
+++ src/components/sections/portfolio.tsx
@@ -12,7 +12,7 @@ import { Pagination, PaginationContent, PaginationItem, PaginationLink, Paginati
 const PROJECTS_PER_PAGE = 6;
 
 const ProjectCard = ({ project }: { project: typeof projects[0] }) => (
-  <Card className="h-full flex flex-col overflow-hidden transform hover:-translate-y-1 transition-transform duration-300">
+  <Card className="h-full flex flex-col overflow-hidden transform hover:-translate-y-1 transition-all duration-300 hover:shadow-glow-primary">
     <CardHeader className="p-0">
       <div className="aspect-video relative">
         <Image
