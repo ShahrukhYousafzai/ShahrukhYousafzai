eade699f1d690c99a1a500e3b607219689367db7
Shahrukh Yousafzai <shahrukhhkhan2003@gmail.com>
2025-07-12 00:10:19 +0000

    the size of portfolio items are too huge, please make it small for bette

--- src/components/sections/portfolio.tsx
+++ src/components/sections/portfolio.tsx
@@ -118,7 +118,7 @@ const PaginatedProjects = ({ projects }: { projects: Array<typeof projects[0]>})
 
   return (
     <>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mt-8">
         {paginatedProjects.map((project) => (
           <ProjectCard key={project.title} project={project} />
         ))}
