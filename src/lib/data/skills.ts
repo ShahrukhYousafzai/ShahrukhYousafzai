8ca20362f5a87130b58b538e380d11271ad05fef
Shahrukh Yousafzai <shahrukhhkhan2003@gmail.com>
2025-07-12 17:06:30 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/data/skills.ts
+++ src/lib/data/skills.ts
@@ -12,18 +12,21 @@ import {
   FileCode,
   Bot
 } from "lucide-react";
-import { IconUnity, IconVisualStudio, IconFigma, IconPhotoshop, IconCSharp, IconPython, IconFirebase, IconPhoton, IconJavascript, IconTypescript, IconXaml } from "@/components/icons";
+import { IconUnity, IconVisualStudio, IconFigma, IconPhotoshop, IconCSharp, IconPython, IconFirebase, IconPhoton, IconJavascript, IconTypescript, IconXaml, IconReact, IconDotNet, IconVisualStudioCode } from "@/components/icons";
 
 export const skills = [
   { name: "Unity", icon: IconUnity },
   { name: "C#", icon: IconCSharp },
   { name: "Game Development", icon: Gamepad2 },
   { name: "Game Design", icon: Brush },
+  { name: ".NET", icon: IconDotNet },
   { name: "Android App Development", icon: Smartphone },
   { name: "iOS Development", icon: Smartphone },
   { name: "Visual Studio", icon: IconVisualStudio },
+  { name: "Visual Studio Code", icon: IconVisualStudioCode },
   { name: "Figma", icon: IconFigma },
   { name: "Photoshop", icon: IconPhotoshop },
+  { name: "React", icon: IconReact },
   { name: "HTML", icon: FileCode },
   { name: "CSS", icon: Palette },
   { name: "JavaScript", icon: IconJavascript },
