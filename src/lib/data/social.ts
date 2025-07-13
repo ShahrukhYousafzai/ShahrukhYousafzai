00d42981645c00a84466f349473553b01022bb46
Shahrukh Yousafzai <shahrukhhkhan2003@gmail.com>
2025-07-13 00:55:18 +0000

    On Header and Footer please remove github and linkedin icon instead add

--- src/lib/data/social.ts
+++ src/lib/data/social.ts
@@ -1,9 +1,8 @@
-import { Github, Linkedin } from "lucide-react";
+import { Twitter } from "lucide-react";
 import { IconFiverr, IconUpwork } from "@/components/icons";
 
 export const socialLinks = [
     { name: 'Fiverr', url: 'https://www.fiverr.com/users/shahrukhkhan9/', icon: IconFiverr },
     { name: 'Upwork', url: 'https://www.upwork.com/freelancers/~01d270e621d61cf22f', icon: IconUpwork },
-    { name: 'GitHub', url: 'https://github.com', icon: Github },
-    { name: 'LinkedIn', url: 'https://linkedin.com', icon: Linkedin },
+    { name: 'Twitter', url: 'https://x.com/SRKYousafzai', icon: Twitter },
 ];
