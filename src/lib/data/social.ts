b459b2953fe8912e188e7ada4de41b3836e89131
Shahrukh Yousafzai <shahrukhhkhan2003@gmail.com>
2025-08-13 03:08:04 +0000

    you have added wrong X/Twitter Link of mine please update it to https://

--- src/lib/data/social.ts
+++ src/lib/data/social.ts
@@ -4,5 +4,5 @@ import { IconFiverr, IconUpwork } from "@/components/icons";
 export const socialLinks = [
     { name: 'Fiverr', url: 'https://www.fiverr.com/users/shahrukhkhan9/', icon: IconFiverr },
     { name: 'Upwork', url: 'https://www.upwork.com/freelancers/~01d270e621d61cf22f', icon: IconUpwork },
-    { name: 'Twitter', url: 'https://x.com/SRKYousafzai', icon: Twitter },
+    { name: 'Twitter', url: 'https://x.com/SRKYousafzaiPK', icon: Twitter },
 ];
