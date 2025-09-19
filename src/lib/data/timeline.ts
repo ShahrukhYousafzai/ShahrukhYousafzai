06ef8f49e312d13343ff3fe9d6c9a4d4d64f0161
Shahrukh Yousafzai <shahrukhhkhan2003@gmail.com>
2025-09-19 11:04:35 +0000

    I have done BS Digital Marketing (not BBA)

--- src/lib/data/timeline.ts
+++ src/lib/data/timeline.ts
@@ -3,5 +3,5 @@ import { Briefcase, School, Cpu } from "lucide-react";
 export const timeline = [
     { date: "2017 - Present", title: "Freelance Developer", description: "Started my professional journey as a freelancer, working with clients worldwide on various development projects.", icon: Briefcase },
     { date: "2019 - 2024", title: "Founder & Game Developer", description: "Founded my own game company, Efface Studios, leading development and creating a wide range of games.", icon: Briefcase },
-    { date: "2021 - 2025", title: "Bachelor of Business Administration (BBA)", description: "Islamia University of Bahawalpur", icon: School },
+    { date: "2021 - 2025", title: "BS Digital Marketing", description: "Islamia University of Bahawalpur", icon: School },
 ];
